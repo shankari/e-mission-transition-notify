595d881569f30c826db7ad44ec519bf7227de7c4
Shankari <shankari@eecs.berkeley.edu>
2020-07-19 16:15:45 -0700

    Return the first transition correctly
    Since Apr 2016, when
    https://github.com/e-mission/cordova-usercache/commit/52d519f02889abeb6fc34b7325121a210e1cb515
    was committed, results from the usercache have been sorted in descending order
    by write_ts.
    
    So when we read the last two transitions at the end of a trip, the first one is
    the start transition. But for some reason, we were returning the second one `[1]`.
    This has been true since https://github.com/e-mission/e-mission-transition-notify/commit/6e70fc8c03a697e8d7c34cff808bf93094560cca
    so I don't know how this ever worked. Maybe the timing has changed now so
    before this the transitions were the ending one + starting one, and with the
    FSM changes, it is now starting + previous end.
    
    Let's change this for now and test some more on real phones.

--- src/android/TransitionNotificationReceiver.java
+++ src/android/TransitionNotificationReceiver.java
@@ -332,7 +332,7 @@ public class TransitionNotificationReceiver extends BroadcastReceiver {
          * Small if - that's all we need
          */
         if (lastTwoTransitions.length == 2) {
-            return lastTwoTransitions[1];
+            return lastTwoTransitions[0];
         } else {
             if (BuildConfig.DEBUG) {
                 if (lastTwoTransitions.length > 2) {
